bca58cd82197a42abc1c6975fd49dbd079037081
Szymon Lesisz <sz.lesisz@gmail.com>
2022-04-06 08:47:09 +0200

    chore(connect): add entrypoints (indexes)

---
+++ packages/connect/src/exports.ts
@@ -0,0 +1,5 @@
+// NOTE: exports should be the same in here and index-browser.ts
+export * as ProtobufMessages from '@trezor/transport/lib/types/messages';
+export * from './constants';
+export * from './events';
+export * from './types';
